57dab1b55ef795992ecc8dd802db4f6e67365a3c
giwoong <dev.choi0409@gmail.com>
2025-10-16 13:10:11 +0900

    feat: Number 예외 처리
    - 숫자가 음수인 경우
    - 숫자가 문자인 경우 (파싱 X)

--- src/main/java/calculator/domain/Number.java
+++ src/main/java/calculator/domain/Number.java
@@ -2,6 +2,9 @@ package calculator.domain;
 
 public class Number {
 
+    private static final String NOT_NUMBER_MESSAGE = "입력값 '%s'는(은) 유효한 숫자가 아닙니다.";
+    private static final String NOT_POSITIVE_MESSAGE = "음수('%d')는 계산할 수 없습니다.";
+
     private final int value;
 
     public Number(String input) {
@@ -14,13 +17,13 @@ public class Number {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("입력값은 숫자여야 합니다.");
+            throw new IllegalArgumentException(String.format(NOT_NUMBER_MESSAGE, input));
         }
     }
 
     private void validatePositive(int number) {
         if (number < 0) {
-            throw new IllegalArgumentException("음수는 계산할 수 없습니다.");
+            throw new IllegalArgumentException(String.format(NOT_POSITIVE_MESSAGE, number));
         }
     }
 
