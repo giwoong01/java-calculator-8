8d45727dcfe1b712c4282637b0360a6de23e8ddd
giwoong <dev.choi0409@gmail.com>
2025-10-16 10:43:06 +0900

    feat: 계산기 구동 기능 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,4 +1,27 @@
 package calculator.controller;
 
+import calculator.domain.Calculator;
+import calculator.domain.ExpressionParser;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class CalculatorController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public CalculatorController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        String expression = inputView.inputExpression();
+
+        Calculator calculator = new Calculator(new ExpressionParser());
+        int sum = calculator.add(expression);
+
+        outputView.outputResult(sum);
+    }
+
 }
